34685a8e7d72bb7be963ea7f248b51fb48e2449a
mrtjoker <game_nitchan12@msn.com>
2016-09-27 23:29:34 +0700

    edit 0-15

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -5,6 +5,11 @@ function Tennis(){
     if(this.scoreA === 0 && this.scoreB === 0 ){
       return 'Love - Love';
     }
+    if(this.scoreA === 0){
+      if(this.scoreB === 15){
+       return 'Love - fifteen';
+     }
+    }
   };
 }
 describe('tannisgame', function() {
@@ -13,5 +18,11 @@ describe('tannisgame', function() {
     tennis.scoreA = 0;
     tennis.scoreB = 0;
     expect(tennis.echo()).toEqual('Love - Love');
+  });
+  it('should be "Love - fifteen" when scoreB get points', function() {
+    var tennis = new Tennis();
+    tennis.scoreA = 0;
+    tennis.scoreB = 15;
+    expect(tennis.echo()).toEqual('Love - fifteen');
   });
 });
