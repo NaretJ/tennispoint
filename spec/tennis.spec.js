104db085392064a3e732123170e11e9e009a07ea
NarezTwentyFour <Narez.Jaroenphum@gmail.com>
2016-09-28 14:14:32 +0700

    Edit class Add playerAgetscore,playerBgetscore,edit echo

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -1,28 +1,56 @@
 function Tennis(){
-    this.scoreA = '';
-    this.scoreB = '';
+    this.scoreA = 0;
+    this.scoreB = 0;
+    this.playerAGetScore = function(){
+      if(this.scoreA === 0 || this.scoreA === 15){
+        this.scoreA += 15;
+      }
+      else if(this.scoreA === 30){
+        this.scoreA += 10;
+      }
+      else if(this.scoreA === 40){
+        this.scoreA = 'game';
+      }
+    };
+    this.playerBGetScore = function(){
+      if(this.scoreB === 0 || this.scoreB === 15){
+        this.scoreB += 15;
+      }
+      else if(this.scoreB === 30){
+        this.scoreB += 10;
+      }
+      else if(this.scoreB === 40){
+        this.scoreB = 'game';
+      }
+    };
     this.echo = function(){
-    if(this.scoreA === 0 && this.scoreB === 0 ){
-      return 'Love - Love';
-    }
-    if(this.scoreA === 0){
-      if(this.scoreB === 15){
-       return 'Love - fifteen';
-     }
-    }
-  };
+      this.score = '';
+      if(this.scoreA === 0 ){
+        this.score += 'Love - ';
+        if(this.scoreB === 0 ){
+          this.score += 'Love';
+        }
+        else if (this.scoreB === 15) {
+          this.score += 'Fifteen';
+        }
+        else if (this.scoreB === 30) {
+          this.score += 'Thirty';
+        }
+        else if (this.scoreB === 40) {
+          this.score += 'Forty';
+        }
+      }
+      return this.score;
+    };
 }
 describe('tannisgame', function() {
   it('should be "Love - Love" when starting the game', function() {
 	  var tennis = new Tennis();
-    tennis.scoreA = 0;
-    tennis.scoreB = 0;
     expect(tennis.echo()).toEqual('Love - Love');
   });
-  it('should be "Love - fifteen" when scoreB get points', function() {
+  it('should be "Love - fifteen" when playerB get points', function() {
     var tennis = new Tennis();
-    tennis.scoreA = 0;
-    tennis.scoreB = 15;
-    expect(tennis.echo()).toEqual('Love - fifteen');
+    tennis.playerBGetScore();
+    expect(tennis.echo()).toEqual('Love - Fifteen');
   });
 });
