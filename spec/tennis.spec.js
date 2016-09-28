a6013341a3463cf1ff5ab18fb075706b047b4339
NarezTwentyFour <Narez.Jaroenphum@gmail.com>
2016-09-28 14:40:54 +0700

    add new condition in func.echo

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -57,6 +57,9 @@ function Tennis(){
         else if (this.scoreB === 40) {
           this.score += 'Forty';
         }
+        else if (this.scoreB === 'game') {
+          this.score = 'player B won';
+        }
       }
       return this.score;
     };
